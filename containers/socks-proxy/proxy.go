21fbe34d2f458cfc705a8ba63566541cc7f29d4b
Cameron <cameron@cam-macbook.goong.vip>
2020-01-05 08:41:41 +0800

    fix: socks proxy on all hosts

--- containers/socks-proxy/proxy.go
+++ containers/socks-proxy/proxy.go
@@ -10,7 +10,7 @@ func main() {
 	}
 
 	// Create SOCKS5 proxy on localhost port 1080
-	if err := server.ListenAndServe("tcp", "127.0.0.1:1080"); err != nil {
+	if err := server.ListenAndServe("tcp", "0.0.0.0:1080"); err != nil {
 		panic(err)
 	}
 }
